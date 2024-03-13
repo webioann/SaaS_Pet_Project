a7dd5b30ba05ed508bbb3374dfbf6e79322d84f1
Webioann <ioannn7777@gmail.com>
2024-03-13 16:13:56 +0200

    used App Router but not Page Router variant

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,12 @@
-import React from 'react';
+import React from 'react'
+import { Roboto } from 'next/font/google'
+
+// If loading a variable font, you don't need to specify the font weight
+const roboto = Roboto({ weight: '300', display: 'swap', subsets: ['latin'] })
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
-        <html lang="en">
+        <html lang="en" className={roboto.className}>
             <body>{children}</body>
         </html>
     )
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function Home() {
     return (
-        <h1>HELLO WORLD AND GIT</h1>
+        <main>
+            <h1>HELLO WORLD AND GIT</h1>
+        </main>
     )
 }
 
