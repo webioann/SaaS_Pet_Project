5442469ef75fcebea424d46add8764e72f982821
Webioann <ioannn7777@gmail.com>
2024-03-13 20:14:17 +0200

    add metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import { Roboto } from 'next/font/google'
+import type { Metadata } from 'next'
 import './global.scss'
 
 // If loading a variable font, you don't need to specify the font weight
 const roboto = Roboto({ weight: '300', display: 'swap', subsets: ['latin'] })
+export const metadata: Metadata = {
+    title: 'next js',
+    description: 'description for metadata',
+}
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
