946a39b362fce1d9b4b25818b6d90963fbc834fd
Webioann <ioannn7777@gmail.com>
2024-07-22 09:35:14 +0300

    was refactored register route Post-method - made more shorted

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -1,17 +1,16 @@
-import connect from "../../../../lib/connect";
+import connect from "../../../../lib/connect"
 import User from '../../../../models/UserSchema'
-import { NextResponse } from "next/server";
-import bcrypt from "bcryptjs";
+import { NextResponse } from "next/server"
+import bcrypt from "bcryptjs"
+import { PassedDataType } from "../../../../types/auth.types"
 
 export async function POST(request) {
   try {
-    const { name, email, password, image, provider } = await request.json();
+    const { name, email, password, image, provider } = await request.json() as PassedDataType
     //check if the user already exists in the MongoDB
-    if(provider === 'credentials') {
       await connect();
       const user = await User.findOne({ email }) 
-  
-      if (user) {
+      if (user && provider === 'credentials') {
         const passwordsMatch = await bcrypt.compare(password, user.password)
         // if User was authenticated and stored on MongoDB earlier
         if(passwordsMatch) {
@@ -25,44 +24,25 @@ export async function POST(request) {
             { status: 501 });
         }
       }
+      if (user && provider === 'google') {
+        return NextResponse.json({
+          message: `User with this email ( ${email} ) already exists in the MongoDB` },
+          { status: 201 });
+      }
       // create a fully new User
       if(!user) {
         const hashedPassword = await bcrypt.hash(password, 10)
         await User.create({ 
           name, 
           email, 
-          password: hashedPassword,
+          password: provider === 'google' ? 'password' : hashedPassword,
           image, 
-          provider: 'credentials' 
+          provider 
         });
         return NextResponse.json({
           message: `New User ${ name ? name : email } Created Successfully` },
           { status: 201 });
       }
-    }
-    if(provider === 'google') {
-      await connect();
-      const user = await User.findOne({ email }) 
-  
-      if (user) {
-          return NextResponse.json({
-            message: `User with this email ( ${email} ) already exists in the MongoDB` },
-            { status: 201 });
-      }
-      if(!user) {
-        await User.create({ 
-          name, 
-          email, 
-          password, 
-          image, 
-          provider: 'google' 
-        });
-        return NextResponse.json({
-          message: `New User ${ name ? name : email } Created Successfully` },
-          { status: 201 });
-      }
-  
-    }
   } catch (error) {
     return NextResponse.json({ 
       error, 
